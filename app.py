5411c13974bb5e0fe2b341cddd47e2b1de1756e0
Amit <amit.sahu89@gmail.com>
2016-11-29 21:31:54 +0100

    added overfit train to work with arbitrary dataset

--- app.py
+++ app.py
@@ -102,8 +102,34 @@ def sample_code():
     return
 
 
-def overfit_model(train_generator, test_generator):
-    
+def overfit_model(train_generator, no_train_instances, test_generator, no_test_instances):
+    # Build the neural-network. Here we'll do a simple 3 layer fully connected network
+
+    model = Sequential()
+    model.add(Dense(512, input_shape=(784,)))
+    model.add(Activation('relu'))  # An "activation" is just a non-linear function applied to the output
+    # of the layer above. Here, with a "rectified linear unit",
+    # we clamp all values below 0 to 0.
+
+    model.add(Dropout(0.2))  # Dropout helps protect the model from memorizing or "overfitting" the training data
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(10))
+    model.add(Activation('softmax'))  # This special "softmax" activation among other things,
+    # ensures the output is a valid probaility distribution, that is
+    # that its values are all non-negative and sum to 1.
+
+    # loss and optimizer
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categorical_accuracy'])
+
+    model.fit_generator(train_generator, no_train_instances, nb_epoch=1, verbose=1, callbacks=[], validation_data=test_generator)
+
+    score = model.evaluate_generator(test_generator, no_test_instances)
+    # print('test loss', score)
+    print('Test score:', score[0])
+    print('Test accuracy:', score[1])
+
     return
 
 def train_model():
